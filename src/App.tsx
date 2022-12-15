8b98b146ec10591de44bec0eeae01cc1b4f1862a
Mehdi <mehdi.zehri@epitech.eu>
2022-12-15 10:00:21 +0100

    feat(theme): add new theme for the entire web-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import Menu from './components/Menu';
+import Home from './pages/Home';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './assets/css/style.css';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#a30b47',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    'fontFamily': '"Roboto", "Helvetica", "Arial", sans-serif',
+  }
+});
 
 function App() {
   return (
-    <Menu />
+    <ThemeProvider theme={theme}>
+      <Menu>
+        <Home />
+      </Menu>
+    </ThemeProvider>
   );
 }
 
